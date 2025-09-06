bedac9bf2de7b072be280f0047f39af010aa6bd3
p-isabella <iisabellappessoa@gmail.com>
2025-09-06 12:41:03 -0300

    commit do projeto pronto!

--- projeto.py
+++ projeto.py
@@ -1,101 +1,81 @@
-'''Os passos a serem inicialmente implementados são:
- (1) desmembrar (parse) a entrada para obter o comando e seus parâmetros
- (2) criar um objeto ProcessBuilder
- (3) iniciar o processo
- (4) obter o fluxo de saída
- (5) enviar o conteúdo retornado pelo comando'''
-
 import os
-import subprocess
+from prompt_toolkit import prompt
+from prompt_toolkit.application import run_in_terminal
+from prompt_toolkit.key_binding import KeyBindings
+
+HistoricoArray = []
+atalhos = KeyBindings() 
+i = 0
+@atalhos.add("up")
 
 # Cria uma função main onde:
 def main():
 
+    run_in_terminal(buffer)
     # Diretório atual é atribuído para uma variável.
     diretorioAtual = os.getcwd()
 
 # Entra em um looping para repetir a entrada de comando até o usuário mandar o programar parar 
     while True:
-
-        itensDiretorio = os.listdir(diretorioAtual)
+        itensDiretorio = os.listdir()
 
     # Aqui, ele cria uma variável para pegar o comando em caixa baixa do usuário, para que ele possa realizar outros processos, como a navegação e o subprocess.    
         comando = input(f'\nGIM {diretorioAtual}>> ').lower().strip()
-
-        if comando == "" or comando == "cd.".strip():
+        
+        # ignora
+        if comando == "":
             continue
 
-    # Lista todas as pastas do diretório atual
+        # volta diretório
+        if comando == "cd ..":
+            os.chdir('..')
+            diretorioAtual = os.getcwd()
+            HistoricoArray.append(comando)
+            print(HistoricoArray)
+
+        # lista diretórios
         elif comando == "dir".strip():
-            print("O volume na unidade C não tem nome.")
-            print("O Número de Série do Volume é não sei ainda como faz\n")
+            HistoricoArray.append(comando)
             for item in itensDiretorio:
                 print(item)
+            print(HistoricoArray)
 
-      # Só printa o repositório atual e não muda mais nada
-        elif comando == "cd".strip():
-            print(diretorioAtual)
-        
-    # Volta para o repositório anterior do atual
-        elif comando == "cd..":
-            os.chdir("..")
-            diretorioAtual = os.getcwd()
-        
-    #Troca o diretório para a pasta que o usuário digitou
+        # muda diretorio para outro específico
         elif comando.startswith("cd "): 
-            novoDiretorio = comando[3:]
-            os.chdir(novoDiretorio)
-            diretorioAtual = os.getcwd()
+            novoDiretorio = comando[3:].strip()
 
-    # Caso o comando for "Sair", ele acabará a repetição.    
-        elif comando == "sair".lower():
-            print("Saindo...")
-            break
-    
-    # !!!!!!!!!!! Importante>>>>>> Aqui ele precisaria de uma condição para navegar no diretório através da biblioteca OS. 
-    # Tipo, se você rodar ele agora, ele só vai listar o diretório atual, que no meu caso é a pasta do Visual Code. Então é preciso manipulação de diretório aqui!
+            try:
+                os.chdir(novoDiretorio)
+                diretorioAtual = os.getcwd()
+                HistoricoArray.append(comando)
+                print(HistoricoArray)
 
-    # Creio eu que o buffer poderia ser chamado após essa estrutura de navegação.
-    
+            except:
+                print(f"Diretório não encontrado: {novoDiretorio}")
 
-    # Aqui, ele pega a função parse e exibe no terminal. Só para mostrar que a lista foi feita.
+        elif comando == os.system(comando):
+            os.system(comando)
+            HistoricoArray.append(comando)
+            print(HistoricoArray)
+        
         else:
-            parse(comando)
-            resultado = parse(comando)
-            print(resultado)
+            print("Comando não encontrado.")
+        
+        # sair
+        if comando in ['sair', 'quit']:
+            print("Fim da execução do shell G.I.M")
+            break
 
+def buffer():
 
-# Essa função retorna o comando em formato de lista: ["tipo", "assim"]
-def parse(comando: str):
-    comandoLista = comando.split()
-    return comandoLista
+    global i
 
-# Essa função puxa o comandoLista e realiza o Subprocess/ProcessBuilder, onde ele vai observar o primeiro item da lista como um comando e os próximos itens do comandoLista como parâmetros.
-def execucaoComando(comandoLista):
-    try:
-        processo = subprocess.Popen(comandoLista, stdout=subprocess.PIPE,
-                                stderr=subprocess.STDOUT, text=True)
-    except FileNotFoundError:
-        return "O comando não foi encontrado."
+    if i == len(HistoricoArray):
+        i = 0
 
-    # O comando try vai tentar (avá) criar um subprocesso (Subprocesso é um processo do sistema) a partir dos seguintes argumentos: A lista de comandos, a saída (que recebe .PIPE = onde permite o redirecionamento de uma resposta 
-    # para outra entrada, e stderr mescla mensagens de erro (como quando a gente testa e dá o erro em vermelho), junto do stdout. Ou seja, todo comando, seja ele certo ou errado, vai pro mesmo lugar.)
-    # Se o comando não for encontrado, não vai funcionar.
+    print(HistoricoArray[0+i])
+    i += 1
 
-
-    # Aqui, ele lê a captura do processo que fizemos e através do .communicate, espera o código terminar. Porém, se o comando demorar mais de 10 segundos para rodar, ele faz .kill(), que 
-    # intervém e "mata" o processo.
-
-    try:
-        out, _ = processo.communicate(timeout=10)
-
-    except subprocess.TimeoutExpired:
-        processo.kill()
-        # aqui ele recebe a saída de out, esse _ ignora outra saída. 
-        out, _ = processo.communicate()
-        out = out + "Processo não finalizado no tempo."
-    
-    return out
-
+    run_in_terminal(buffer)
 
 main()

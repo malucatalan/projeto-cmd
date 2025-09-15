12ca5e86b77446a413c940a4d9ec8630eb36f226
malucatalan <mariapscatalan@gmail.com>
2025-09-15 15:30:28 -0300

    commit 2

--- projeto.py
+++ projeto.py
@@ -18,7 +18,6 @@ def main():
 # Entra em um looping para repetir a entrada de comando até o usuário mandar o programar parar 
     while True:
         itensDiretorio = os.listdir()
-
     # Aqui, ele cria uma variável para pegar o comando em caixa baixa do usuário, para que ele possa realizar outros processos, como a navegação e o subprocess.    
         comando = input(f'\nGIM {diretorioAtual}>> ').lower().strip()
         
@@ -26,19 +25,31 @@ def main():
         if comando == "":
             continue
 
+        elif comando == 'task':
+            print("DIR                             Lista todos os itens no diretório atual.")
+            print("CD <DIRETORIO>                  Muda para o diretório especificado.")
+            print("CD ..                           Volta para o diretório anterior.")
+            print("HISTORY                         Mostra o histórico de comandos.")
+            print("TASKS                           Mostra esta lista de comandos.")
+            print("EXIT                            Finaliza o shell G.I.M.")
+            HistoricoArray.append(comando)
+
+        # lista o histórico de comandos chamados
+        elif comando in ['history']:
+            for item in HistoricoArray:
+                print(item)
+
         # volta diretório
-        if comando == "cd ..":
+        elif comando == "cd ..":
             os.chdir('..')
             diretorioAtual = os.getcwd()
             HistoricoArray.append(comando)
-            print(HistoricoArray)
 
         # lista diretórios
         elif comando == "dir".strip():
             HistoricoArray.append(comando)
             for item in itensDiretorio:
                 print(item)
-            print(HistoricoArray)
 
         # muda diretorio para outro específico
         elif comando.startswith("cd "): 
@@ -48,35 +59,34 @@ def main():
                 os.chdir(novoDiretorio)
                 diretorioAtual = os.getcwd()
                 HistoricoArray.append(comando)
-                print(HistoricoArray)
 
             except:
                 print(f"Diretório não encontrado: {novoDiretorio}")
 
-        elif comando == os.system(comando):
-            os.system(comando)
-            HistoricoArray.append(comando)
-            print(HistoricoArray)
-        
-        else:
-            print("Erro ao executar o comando.")
-        
-        # sair
-        if comando in ['sair', 'quit']:
+        # sair 
+        elif comando in ['exit']:
             print("Fim da execução do shell G.I.M")
             break
 
-def buffer():
+        # condição de erro para comandos inválidos
+        else:
+            print(f"'{comando}' não é um comando válido")
+            HistoricoArray.append(comando)
 
-    global i
+    # criar a função buffer onde:
+    def buffer():
 
-    if i == len(HistoricoArray):
-        i = 0
+        # acessa a variável global i
+        global i
 
-    print(HistoricoArray[0+i])
-    i += 1
+        if i == len(HistoricoArray):
+            i = 0
 
-    run_in_terminal(buffer)
+        # printa o comando atual
+        print(HistoricoArray[0+i])
+        i += 1
 
-main()
+        run_in_terminal(buffer)
 
+    # inicia o programa chamando a função main
+    main()
